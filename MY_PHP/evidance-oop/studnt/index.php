b35a4f3d055f5a5b201484637119c03befa842d9
Md. Abdullah-Al-Mamun <152875087+sheikh-mamun@users.noreply.github.com>
2024-03-02 19:01:31 +0600

    new

--- MY_PHP/evidance-oop/studnt/index.php
+++ MY_PHP/evidance-oop/studnt/index.php
@@ -1,62 +1,54 @@
-<?php
-//Step 2:
-require_once("student_class.php");
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Student Form</title>
+</head>
+<body>
 
-if(isset($_POST["btnSubmit"])){
-	
-	$id=$_POST["txtId"];
-	$name=$_POST["txtName"];
-	$course=$_POST["txtCourse"];
-	$phone=$_POST["txtPhone"];	
-	
-	if(preg_match("/^[0-9+]{11,14}$/",$phone)){		
-   
-		$student=new Student($id,$name,$course,$phone); 	
-		$student->save();
-		echo "Success!";
-	
-    }else{
-	   echo "Invalid Phone";	 
+<form action="#" method="post">
+    <div>
+        ID:<br/>
+        <input type="text" name="txtId"/>
+    </div>
+
+    <div>
+        Name<br/>
+        <input type="text" name="txtName"/>
+    </div>
+
+    <div>
+        Course<br/>
+        <input type="text" name="txtCourse"/>
+    </div>
+
+    <div>
+        Phone<br/>
+        <input type="text" name="txtPhone"/>
+    </div>
+
+    <div>
+        <input type="submit" name="btnSubmit" value="Submit"/>
+    </div>
+</form>
+
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $id = $_POST["txtId"];
+    $name = $_POST["txtName"];
+    $course = $_POST["txtCourse"];
+    $phone = $_POST["txtPhone"];
+
+    if (preg_match("/^[0-9+]{11,14}$/", $phone)) {
+        $data = "$id,$name,$course,$phone\n";
+        file_put_contents("data.txt", $data, FILE_APPEND);
+        echo "Success!";
+    } else {
+        echo "Invalid Phone";
     }
 }
 ?>
 
-<!doctype html>
-<html>
-<head>
-<meta charset="utf-8">
-<title>Untitled Document</title>
-</head>
-
-<body>
-
-<form action="#" method="post">
-<div>
-ID:<br/>
-<input type="text" name="txtId" />
-</div>
-
-<div>
-Name<br/>
-<input type="text" name="txtName" />
-</div>
-
-<div>
-Course<br/>
-<input type="text" name="txtCourse" />
-</div>
-
-<div>
-Phone<br/>
-<input type="text" name="txtPhone" />
-</div>
-
-<div>
-<input type="submit" name="btnSubmit" value="Submit" />
-</div>
-</form>
-<?php
- Student::display_students();
-?>
 </body>
 </html>
--- MY_PHP/evidance-oop/studnt/student_class.php
+++ MY_PHP/evidance-oop/studnt/student_class.php
@@ -1,55 +1,42 @@
 <?php
-//Step 1:
-class Student{   
-   private $id;
-   private $name;
-   private $course;
-   private $phone;  
-   
-   private static $file_path="data.txt"; 
-   
-   //---------------constructor---------------//
-   function __construct($_id,$_name,$_course,$_phone){
-	   $this->id=$_id;
-	   $this->name=$_name;
-	   $this->course=$_course;
-	   $this->phone=$_phone;
-   }
-  
-  //---------------csv function-------------------//
-   public function csv(){
-	 return $this->id.",".$this->name.",".$this->course.",".$this->phone.PHP_EOL;   
-   }
-   
-   //-----------save function-----------------//
-   public function save(){
-	   
-	       $students=file(self::$file_path);  	   
-	  	
-		   file_put_contents(self::$file_path,$this->csv(),FILE_APPEND);
-	   	   
-	   
-   }//end save	
-       
-   
-   //---------------display_students-------------//
-   
-   public static function display_students(){
-	   
-	    $students=file(self::$file_path);
-		
-		echo "<b>ID | Name | COURSE | PHONE</b><br/>";
-		foreach($students as $student){
-				   list($id,$name,$course,$phone)=explode(",",trim($student));
-				   echo "$id | $name | $course | $phone<br/>";
-				   
-	    }
-				
-		
-   }   
-   
- //-----------------end functions----------------   
 
-}// Student class
+class Student {
+    private $id;
+    private $name;
+    private $course;
+    private $phone;
+
+    private static $file_path = "data.txt";
+
+    public function __construct($id, $name, $course, $phone) {
+        $this->id = $id;
+        $this->name = $name;
+        $this->course = $course;
+        $this->phone = $phone;
+    }
+
+    public function save() {
+        file_put_contents(self::$file_path, $this->toCsv(), FILE_APPEND);
+    }
+
+    private function toCsv() {
+        return "{$this->id},{$this->name},{$this->course},{$this->phone}" . PHP_EOL;
+    }
+}
+
+if ($_SERVER["REQUEST_METHOD"] === "POST") {
+    $id = $_POST["txtId"];
+    $name = $_POST["txtName"];
+    $course = $_POST["txtCourse"];
+    $phone = $_POST["txtPhone"];
+
+    if (preg_match("/^[0-9+]{11,14}$/", $phone)) {
+        $student = new Student($id, $name, $course, $phone);
+        $student->save();
+        echo "Success!";
+    } else {
+        echo "Invalid Phone";
+    }
+}
 
 ?>
